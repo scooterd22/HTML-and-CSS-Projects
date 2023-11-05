a02406e6f7cef634caa0e1b911d8d5e8469bc66a
Scott DiBenedetto <dibenedettoscott@gmail.com>
2023-11-04 20:56:53 -0700

    uploading after basic css

---
+++ New folder/hello_world.js
@@ -0,0 +1,8 @@
+var greeting = 'hello world!';
+console.log(greeting); 
+
+var greeting2 = 'welcome to node.js';
+console.log(greeting2);
+
+var sentence1 = 'learning how to debug code with the debugger';
+console.log(sentence1);
